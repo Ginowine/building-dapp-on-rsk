67aef5916899a73ee5f1d36628be1652d16ced9d
Gino <gino.osahon@gmail.com>
2021-10-12 08:27:06 +0100

    first commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -17,23 +17,34 @@ contract Election {
 
     constructor () public {
         // TODO set default candidates
+        addCandidate("Carrot");
+        addCandidate("Potato");
     }
 
     function addCandidate (
         string memory _name
     ) private {
         // TODO store new candidate in state variables
+                candidatesCount++;
+        candidates[candidatesCount] =
+            Candidate(candidatesCount, _name, 0);
     }
 
     function vote (
-        uint _candidateId
+ uint _candidateId
     ) public {
         // TODO check that account hasn't voted before
-        // and that candidate is valid
+        require(
+            !voters[msg.sender]);
+        // require a valid candidate
         require(
             _candidateId > 0 &&
             _candidateId <= candidatesCount);
 
         // TODO record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
     }
 }
